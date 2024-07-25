83fd7eb101a5691e571f9e27b67b55a21f5e3279
cpcO7 <urunovsamariddin@gmail.com>
2024-07-25 15:30:58 +0500

    asd

--- root/settings.py
+++ root/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -105,6 +106,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR / "static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
